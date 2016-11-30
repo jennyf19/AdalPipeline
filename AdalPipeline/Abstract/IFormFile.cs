89cb2fa88518dba68cebf8b684d37c5ef3fcfc87
jennyf19 <ferriesj@gmail.com>
2016-11-30 14:56:11 -0800

    Changes to views

---
+++ AdalPipeline/Abstract/IFormFile.cs
@@ -0,0 +1,24 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel.DataAnnotations;
+using System.IO;
+using System.Linq;
+using System.Text;
+using System.Threading;
+using System.Threading.Tasks;
+
+namespace AdalPipeline.Abstract
+{
+    public interface IFormFile
+    {
+        string ReleaseTitle { get; }
+        string ReleaseVersion { get; }
+
+        [DataType(DataType.MultilineText)]
+        string RelaseNotes { get; }
+
+        Stream OpenReadStream();
+        void CopyTo(Stream target);
+        Task CopyToAsync(Stream target);
+    }
+}
--- AdalPipeline/Controllers/HomeController.cs
+++ AdalPipeline/Controllers/HomeController.cs
@@ -3,6 +3,7 @@ using System.Collections;
 using System.Collections.Generic;
 using System.IO;
 using System.Linq;
+using System.Text;
 using System.Text.RegularExpressions;
 using System.Web;
 using System.Web.Mvc;
@@ -48,17 +49,43 @@ namespace AdalPipeline.Controllers
             return View();
         }
 
-        public ActionResult ApproveRequest(Request request)
+        
+        public ViewResult ApproveRelease(Request request)
         {
-           //ViewData["ReleaseTitle"] = request.ReleaseTitle;
-            //ViewData["ReleaseVersion"] = request.ReleaseVersion;
-            //ViewData["ReleaseNotes"] = request.ReleaseNotes;
-
-            return View("ApproveRequest");
+            return View("ApproveRelease", request);
         }
 
-       
-      
+        /*public ActionResult ApproveRelease(Request request)
+        {
+            return View("ApproveRelease");
+        }*/
+
+        public ActionResult LoadProducts()
+        {
+            List<SelectListItem> productList = new List<SelectListItem>();
+            productList.Add(new SelectListItem { Text = "Select", Value = "0" });
+            productList.Add(new SelectListItem { Text = "Adal-v4", Value = "1" });
+            productList.Add(new SelectListItem { Text = "Azure-activedirectory-library-for-android-convergence", Value = "2" });
+            productList.Add(new SelectListItem { Text = "Azure-activedirectory-library-for-dotnet-v2", Value = "3" });
+            productList.Add(new SelectListItem { Text = "Azure-activedirectory-library-for-dotnet-v3", Value = "4" });
+            productList.Add(new SelectListItem { Text = "Azure-activedirectory-library-for-java", Value = "5" });
+            productList.Add(new SelectListItem { Text = "Azure-activedirectory-library-for-js-dev", Value = "6" });
+            productList.Add(new SelectListItem { Text = "Azure-activedirectory-library-for-js-master", Value = "7" });
+            productList.Add(new SelectListItem { Text = "Azure-activedirectory-library-for-nodejs-master", Value = "8" });
+            productList.Add(new SelectListItem { Text = "Azure-activedirectory-library-for-objc-dev", Value = "9" });
+            productList.Add(new SelectListItem { Text = "build-android-master", Value = "10" });
+            productList.Add(new SelectListItem { Text = "gradle-build-android-master", Value = "11" });
+            productList.Add(new SelectListItem { Text = "msal-dotnet", Value = "12" });
+            productList.Add(new SelectListItem { Text = "NativeADAL", Value = "13" });
+            productList.Add(new SelectListItem { Text = "WilsonForDotNet45Dev", Value = "14" });
+            productList.Add(new SelectListItem { Text = "WilsonForDotNet45Release", Value = "15" });
+            productList.Add(new SelectListItem { Text = "WilsonForKDev", Value = "16" });
+            productList.Add(new SelectListItem { Text = "WilsonForKMaster", Value = "17" });
+            productList.Add(new SelectListItem { Text = "WilsonForKRelease", Value = "18" });
+            ViewData["product"] = productList;
+            return View();
+        }
+
         public ActionResult About()
         {
             ViewBag.Message = "Your application description page.";
@@ -71,6 +98,11 @@ namespace AdalPipeline.Controllers
             ViewBag.Message = "Your contact page.";
 
             return View();
+        }
+
+        public ActionResult Completed()
+        {
+            return View("Completed");
         }
     }
 }
